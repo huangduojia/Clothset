7f13c6a158a30a63ac74e522a798f1d372a5421a
huangduojia <huangduojia@qq.com>
2013-06-08 20:15:00 +0800

    new project
    /

---
+++ Clothset/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clothset
+//
+//  Created by huang on 13-6-8.
+//  Copyright (c) 2013年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
