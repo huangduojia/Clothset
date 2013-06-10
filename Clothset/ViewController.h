75f7dc084becbfc64ae3b7141a35b6599723ba36
GavinIsNobody <gavin@gavintekiMac-mini.local>
2013-06-10 23:49:29 +0800

    Revert "first version done"
    This reverts commit 4c52f9d1f693de2f02d28746ac087f5ecddd5fed.

--- Clothset/ViewController.h
+++ Clothset/ViewController.h
@@ -7,32 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MobileCoreServices/UTCoreTypes.h>
-#import <sqlite3.h>
-
 
 @interface ViewController : UIViewController <UIImagePickerControllerDelegate>
 
 {
     UIButton *GetPhoto;
-    UIImageView *ShotPhoto;
-    UITextField *brands;
-    NSString *databasepath;
-    sqlite3 *clothsetDB;
-    UIImage *image ;
-    int note;
+    UIImageView *viewtest;
+    
+    
 }
 
 
 @property (nonatomic,strong)IBOutlet UIButton *GetPhoto;
-@property (nonatomic,strong) IBOutlet UIImageView *ShotPhoto;
-@property (nonatomic,strong) IBOutlet UITextField *brands;
-static UIImage *ShrinkImage(UIImage *original_image,CGSize size);
-static UIImage *scale(UIImage *image ,CGSize size);
+@property (nonatomic,strong) IBOutlet UIImageView *viewtest;
 -(IBAction)get_a_photo:(id)sender;
--(IBAction)select_to_clothset:(id)sender;
--(IBAction)textfielddoneedit:(id)sender;
--(IBAction)backgroundtap:(id)sender;
--(void) addPicEvent;
+- (void) addPicEvent;
 
 @end
